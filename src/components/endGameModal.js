2a5548dc92c089f3cb7b4db23e64299655286a8c
Thomas Shaw <tshaw@3way.com.ar>
2020-02-12 13:43:17 -0300

    EndGame modal style fix

--- src/components/endGameModal.js
+++ src/components/endGameModal.js
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import {
 	Dialog,
 	DialogContent,
 	DialogTitle,
 	DialogContentText,
 	Button,
+	Typography,
+	Divider,
 } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+	dialogTitle: {
+		textAlign: 'center',
+		//flexGrow: 1,
+	},
+	dialogMain: {
+		//width: 300,
+		//height: 200,
+	},
+	newGameButton: {
+		margin: theme.spacing(3),
+	},
+	dialogContent: {
+		width: theme.spacing(50),
+		textAlign: 'center',
+	},
+}));
+
 export default function EndGameModal({ endedObject }) {
+	const classes = useStyles();
 	const { ended, playerWon } = endedObject;
 	const [open, setOpen] = useState(false);
 	const [reroute, setReroute] = useState(false);
@@ -32,13 +54,26 @@ export default function EndGameModal({ endedObject }) {
 	}
 
 	return (
-		<Dialog open={open} onClose={handleModalClose}>
-			<DialogTitle>Game Ended</DialogTitle>
-			<DialogContent>
-				<DialogContentText>
+		<Dialog
+			open={open}
+			className={classes.dialogMain}
+			onClose={handleModalClose}
+		>
+			<DialogTitle disableTypography className={classes.dialogTitle}>
+				<Typography variant="h4">
 					{playerWon ? 'You won!' : 'You lost!'}
-				</DialogContentText>
-				<Button onClick={handleModalButtonClick}>Play Again?</Button>
+				</Typography>
+			</DialogTitle>
+			<Divider />
+			<DialogContent className={classes.dialogContent}>
+				<Button
+					variant="contained"
+					color="secondary"
+					onClick={handleModalButtonClick}
+					className={classes.newGameButton}
+				>
+					Play Again?
+				</Button>
 			</DialogContent>
 		</Dialog>
 	);
